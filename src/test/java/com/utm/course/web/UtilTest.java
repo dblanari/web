a47c8074754d4e5d7868d2a4342427781880a152
dblanari <denis.blanari@endava.com>
2019-10-28 18:40:32 +0200

    add unit test

---
+++ src/test/java/com/utm/course/web/UtilTest.java
@@ -0,0 +1,18 @@
+package com.utm.course.web;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class UtilTest {
+
+
+  @Test
+  public void testSumPositive() {
+    List<Integer> list = Arrays.asList(1, 2, 3);
+    final Integer sum = Util.sum(list);
+    assertThat(sum).isEqualTo(5);
+  }
+}
