8d9ac952893dde261f3f7e58cc1ae2717dc200b1
dblanari <denis.blanari@endava.com>
2019-12-04 19:17:29 +0200

    test jpa

--- src/main/java/com/utm/course/web/config/SecurityConfig.java
+++ src/main/java/com/utm/course/web/config/SecurityConfig.java
@@ -1,22 +1,22 @@
-package com.utm.course.web.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-        .antMatchers("/", "/public/**", "/webjars/**").permitAll()
-        .anyRequest().authenticated()
-        .and()
-        .oauth2Login()
-        .and().csrf().disable();
-  }
-
-}
+//package com.utm.course.web.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+//    http.authorizeRequests()
+//        .antMatchers("/", "/public/**", "/webjars/**").permitAll()
+//        .anyRequest().authenticated()
+//        .and()
+//        .oauth2Login()
+//        .and().csrf().disable();
+//  }
+//
+//}
